1c50165c80e969bd9937552d47315fbe7f5149ab
jiqishoubi <437753880@qq.com>
2022-01-25 11:56:23 +0800

    update webpack merge

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -1,63 +1,63 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin"); // html模板
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin') // html模板
 const cssLoader = {
-  loader: "css-loader",
+  loader: 'css-loader',
   options: {
     // 启动css modules // antd为global，less文件以.global.less为后缀时为global
     modules: {
-      localIdentName: "[path][name]__[local]__",
+      localIdentName: '[path][name]__[local]__',
       mode: (resourcePath) => {
-        if (
-          resourcePath.indexOf("/node_modules/antd/") > -1 ||
-          resourcePath.indexOf(".global.less") > -1
-        ) {
-          return "global";
+        if (resourcePath.indexOf('/node_modules/antd/') > -1 || resourcePath.indexOf('.global.less') > -1) {
+          return 'global'
         }
-        return "local";
+        return 'local'
       },
     },
   },
-};
+}
 
 const lessLoader = {
-  loader: "less-loader",
+  loader: 'less-loader',
   options: {
     lessOptions: { javascriptEnabled: true },
   },
-};
+}
 
+/**
+ * config
+ */
 module.exports = {
-  mode: "development",
-  entry: "./src/main.js",
+  mode: 'development',
+  entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, "../dist"),
-    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].[contenthash].js',
     clean: true, // 每次构建清理dist
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: "babel-loader",
+        use: 'babel-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.(ts|tsx)$/,
-        use: ["babel-loader", "ts-loader"],
+        use: ['babel-loader', 'ts-loader'],
         exclude: /node_modules/,
       },
       {
         test: /\.(css|less)$/,
-        use: ["style-loader", cssLoader, lessLoader],
+        use: ['style-loader', cssLoader, lessLoader],
       },
       // 编译文件
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
         use: [
           {
-            loader: "url-loader", // 小的文件转成base64
+            loader: 'url-loader', // 小的文件转成base64
             options: {
               limit: 8192,
             },
@@ -67,9 +67,9 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"], // 引入组件的时候可以省略这些后缀
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'], // 引入组件的时候可以省略这些后缀
     alias: {
-      "@": path.resolve(__dirname, "../src"),
+      '@': path.resolve(__dirname, '../src'),
     },
   },
   devServer: {
@@ -77,21 +77,22 @@ module.exports = {
     hot: true,
     port: 9123,
   },
-  devtool: "eval-source-map",
+  devtool: 'eval-source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
+      template: './src/index.html',
+      filename: 'index.html',
     }),
     // 全局自动引入react // 不用每个文件都 import React from 'react'
     new webpack.ProvidePlugin({
-      React: "react",
+      React: 'react',
     }),
     // 把编译时的变量设置到项目里
     new webpack.DefinePlugin({
-      "process.env": {
-        ENV: JSON.stringify(process.env.ENV),
+      'process.env': {
+        MODE: JSON.stringify(process.env.MODE), // 开发环境
+        ENV: JSON.stringify(process.env.ENV), // 请求环境
       },
     }),
   ],
-};
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,5 @@
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const config_base = require('./config/webpack.config.base')
 const config_dev = require('./config/webpack.config.dev')
 const config_prod = require('./config/webpack.config.prod')
-
-console.log(process.env.ENV)
-
-module.exports = config_base
+module.exports = merge(config_base, process.env.MODE == 'production-mode' ? config_prod : config_dev)
